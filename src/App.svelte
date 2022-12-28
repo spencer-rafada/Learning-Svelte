9b255dc994bdbfc420b1d34e2dcceb0d079d30fa
spencer-rafada <spencer.byui@gmail.com>
2022-12-27 22:02:33 -0700

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Nested from "./Nested.svelte";
+	import Info from './Info.svelte';
+	import Conditionals from "./Conditionals.svelte";
+
+	const pkg = {
+		name: 'svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	};
+</script>
+
+<Nested answer={42}/>
+<Nested />
+
+<Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>
+<Info {...pkg} />
+
+<Conditionals />
